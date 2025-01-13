4a4102cea692be66f8e34568b688c9ac3486b7e4
ephemeralRhythm <alijradi2003@gmail.com>
2025-01-13 06:42:34 +0200

    reverted back to working version

--- packages/client/src/pages/login.tsx
+++ packages/client/src/pages/login.tsx
@@ -15,7 +15,7 @@ export const LoginPage: React.FC = () => {
   }, [navigate]);
 
   const handleLogin = () => {
-    const client = new Colyseus.Client("wss://nochessnolife.cc/");
+    const client = new Colyseus.Client("ws://localhost:4070");
 
     client.auth.signInWithProvider("discord").then(() => {
       if (client.auth.token) {
