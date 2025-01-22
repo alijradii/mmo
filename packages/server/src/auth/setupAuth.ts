b6d9009386c86033b4b05edaa9b6452851e4b601
ephemeralRhythm <alijradi2003@gmail.com>
2025-01-22 03:06:30 +0200

    fixed server url on discord auth

--- packages/server/src/auth/setupAuth.ts
+++ packages/server/src/auth/setupAuth.ts
@@ -1,21 +1,20 @@
 import { auth, JWT } from "@colyseus/auth";
 
 export const setupAuth = () => {
-    auth.oauth.defaults.origin = "http://localhost:4070"
-        // "https://nochessnolife.cc" 
-    // || "http://localhost:4070"
-    auth.oauth.defaults.secret = "FDKJLFKDLSJFLKJ*(FJDSK"
+  auth.oauth.defaults.origin = "https://nochessnolife.cc";
+  // || "http://localhost:4070"
+  auth.oauth.defaults.secret = "FDKJLFKDLSJFLKJ*(FJDSK";
 
-    auth.oauth.addProvider('discord', {
-        key: process.env.DISCORD_CLIENT_ID,
-        secret: process.env.DISCORD_CLIENT_SECRET,
-        scope: ['identify'],
-    });
-    
-    auth.oauth.onCallback(async(data, provider)=> {
-        const profile = data.profile; 
-        console.log(profile)
-        
-        return profile;
-    })
-}
+  auth.oauth.addProvider("discord", {
+    key: process.env.DISCORD_CLIENT_ID,
+    secret: process.env.DISCORD_CLIENT_SECRET,
+    scope: ["identify"],
+  });
+
+  auth.oauth.onCallback(async (data, provider) => {
+    const profile = data.profile;
+    console.log(profile);
+
+    return profile;
+  });
+};
