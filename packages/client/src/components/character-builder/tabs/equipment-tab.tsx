e74aa179e1edfbc3f3581f01c91b6acce6d081b9
ephemeralRhythm <alijradi2003@gmail.com>
2025-04-18 20:01:29 +0300

    [feat] [server] added controller to update self user

---
+++ packages/client/src/components/character-builder/tabs/equipment-tab.tsx
@@ -0,0 +1,3 @@
+export const EquipmentTab: React.FC = () => {
+    return <div></div>
+}
--- packages/server/src/controllers/user.controller.ts
+++ packages/server/src/controllers/user.controller.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { PlayerModel } from "../database/models/player.model";
+import { IPlayer, PlayerModel } from "../database/models/player.model";
 import { IMember, MemberModel } from "../database/models/member.model";
 
 import { PlayerComponents } from "../schemas/player/playerComponents";
@@ -44,31 +44,19 @@ const findOrCreatePlayer = async (id: string, username: string) => {
   }
 };
 
-export const editUserGear = async (
-  req: express.Request,
-  res: express.Response
-) => {
-  const id: string = (req as any).auth.id;
-  const username: string = (req as any).auth.username;
-
-  if (!id || !username)
-    res
-      .status(400)
-      .json({ status: "failed", error: "discord profile not found" });
-
+export const validateGear = (gear: { [index: string]: string }) => {
   const {
-    frontextra,
-    head,
-    hair,
-    hat,
-    backhair,
-    top,
-    bottom,
-    weapon,
-  }: { [index: string]: string } = req.body;
+    frontextra = "",
+    head = "",
+    hair = "",
+    hat = "",
+    backhair = "",
+    top = "",
+    bottom = "",
+    weapon = "",
+  } = gear;
 
-  let invalidInput = false;
-  if (!head || !top || !bottom || !weapon) invalidInput = true;
+  if (!head || !top || !bottom || !weapon) return false;
 
   if (
     !PlayerComponents.head.includes(head) ||
@@ -78,30 +66,55 @@ export const editUserGear = async (
     (hat !== "" && !PlayerComponents.hat.includes(hat)) ||
     (frontextra !== "" && !PlayerComponents.frontextra.includes(frontextra)) ||
     (backhair !== "" && !PlayerComponents.backhair.includes(backhair))
-  )
-    invalidInput = true;
-
-  if (invalidInput) {
-    res.status(400).json({ status: "failed", error: "bad request" });
-    return;
+  ) {
+    return false;
   }
 
-  findOrCreatePlayer(id, username);
+  return true;
+};
 
-  const player = new PlayerModel({
-    gear: {
-      frontextra,
-      head,
-      hair,
-      backhair,
-      hat,
-      top,
-      bottom,
-      weapon,
-    },
-  });
+export const validateStats = (user: IPlayer): boolean => {
+  const sum =
+    user.STR +
+    user.DEX +
+    user.CON +
+    user.CHA +
+    user.INT +
+    user.WIS +
+    user.points;
+  return sum === 70;
+};
 
-  await PlayerModel.findOneAndUpdate({ _id: id }, player.toObject());
+export const validateClass = (cl: string): boolean => {
+  return ["warrior", "mage", "rogue", "cleric"].indexOf(cl) !== -1;
+};
+
+export const validateRace = (race: string): boolean => {
+  return ["elf", "human", "dwarf", "orc"].indexOf(race) !== -1;
+};
+
+export const editUserGear = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  const id: string = (req as any).auth.id;
+  const username: string = (req as any).auth.username;
+
+  if (!id || !username)
+    return res
+      .status(400)
+      .json({ status: "failed", error: "discord profile not found" });
+
+  const gear: { [index: string]: string } = req.body;
+
+  const isValid = validateGear(gear);
+
+  if (!isValid) {
+    return res.status(400).json({ status: "failed", error: "bad request" });
+  }
+
+  const player: IPlayer = await findOrCreatePlayer(id, username);
+  await PlayerModel.findOneAndUpdate({ _id: id }, player);
 
   res.json({ status: "success" });
 };
@@ -142,4 +155,54 @@ export const getMe = async (req: express.Request, res: express.Response) => {
     console.error("Error fetching authenticated user:", error);
     res.status(500).json({ message: "Internal server error" });
   }
+};
+
+export const updateMe = async (req: express.Request, res: express.Response) => {
+  const id: string = (req as any).auth.id;
+  const username: string = (req as any).auth.username;
+  const userData = (req as any).data;
+
+  let user: IPlayer | null = null;
+
+  try {
+    if (!id) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    user = await PlayerModel.findById(id);
+  } catch (error) {
+    console.error("Error fetching authenticated user:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+
+  if (!user) return res.status(404).json({ message: "User not found" });
+
+  if (!validateGear(user.gear))
+    return res.status(404).json({ message: "Gear is invalid." });
+
+  if (!validateStats(user))
+    return res.status(404).json({ message: "Ability scores are invalid." });
+
+  if (!validateClass(user.class))
+    return res.status(404).json({ message: "Invalid class." });
+
+  if (!validateRace(user.race))
+    return res.status(404).json({ message: "Invalid race." });
+
+  await PlayerModel.findOneAndUpdate(
+    { _id: id },
+    {
+      STR: user.STR,
+      DEX: user.DEX,
+      INT: user.INT,
+      CON: user.CON,
+      WIS: user.WIS,
+      CHA: user.CHA,
+      points: user.points,
+      class: user.class,
+      race: user.race,
+    }
+  );
+
+  return res.status(200).json({ message: "success" });
 };
