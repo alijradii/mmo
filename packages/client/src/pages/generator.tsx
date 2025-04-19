f9236ba9fe6d81e4b6889d6f16dc13915be9f382
ephemeralRhythm <alijradi2003@gmail.com>
2025-04-19 12:00:25 +0300

    [client] removed weapon from appearance section to be moved to equipment

--- packages/client/src/pages/generator.tsx
+++ packages/client/src/pages/generator.tsx
@@ -12,6 +12,7 @@ import axios from "axios";
 import { useToast } from "@/hooks/use-toast";
 import { useAtom } from "jotai";
 import { userDataAtom } from "@/state/userAtom";
+import { CharacterCard } from "@/components/character-card";
 
 interface DirectionOrder {
   [index: string]: number;
@@ -94,35 +95,7 @@ const initialCategories: Category[] = [
     icon: "backhair1",
     order: { South: 0, North: 10, West: 3 },
   },
-  {
-    name: "Weapon",
-    path: "weapon",
-    primary: [],
-    all: [],
-    icon: "axe1",
-    order: { South: 100, North: 0, West: 100 },
-  },
 ];
-
-type DirectionalDepth = {
-  [index: string]: number;
-};
-
-type ComponentsDepthIndex = {
-  [index: string]: DirectionalDepth;
-};
-
-const componetsDepthIndex: ComponentsDepthIndex = {
-  frontextra: { up: 4, down: 8, left: 8, right: 8 },
-  backextra: { up: 5, down: 1, left: 1, right: 1 },
-  hair: { up: 7, down: 6, left: 6, right: 6 },
-  backhair: { up: 8, down: 2, left: 4, right: 4 },
-  hat: { up: 9, down: 7, left: 7, right: 7 },
-  weapon: { up: 1, down: 9, left: 9, right: 9 },
-  head: { up: 6, down: 5, left: 5, right: 5 },
-  top: { up: 3, down: 4, left: 3, right: 3 },
-  bottom: { up: 2, down: 3, left: 2, right: 2 },
-};
 
 interface IGear {
   frontextra?: string;
@@ -383,48 +356,8 @@ export const GeneratorPage: React.FC = () => {
       </Tabs>
 
       <div className="w-full flex gap-10 justify-between mt-5">
-        {["down", "left", "right", "up"].map((direction, index) => (
-          <Card
-            key={direction}
-            aria-label={direction}
-            className="flex items-center justify-center w-full py-2 bg-zinc-800"
-          >
-            <div
-              className="relative"
-              role="img"
-              aria-label="Character sprite"
-              style={{
-                width: `${frameWidth * scale * 2}px`,
-                height: `${frameHeight * scale * 2}px`,
-              }}
-            >
-              {categories.map((category) => {
-                const swap = userData?.gear?.[category.path];
-                if (!swap) return <div key={category.path + direction} />;
-
-                return (
-                  <div
-                    className="absolute"
-                    key={category.path + direction}
-                    style={{
-                      zIndex: componetsDepthIndex[category.path][direction],
-                      width: `${frameWidth * scale * 2}px`,
-                      height: `${frameHeight * scale * 2}px`,
-                      backgroundImage: `url("/assets/spritesheets/player/${category.path}/${swap}.png")`,
-                      backgroundRepeat: "no-repeat",
-                      backgroundSize: `${1104 * scale * 2}px ${
-                        192 * scale * 2
-                      }px`,
-                      backgroundPosition: `-${48 * scale * 2}px -${
-                        48 * index * scale * 2
-                      }px`,
-                      imageRendering: "pixelated",
-                    }}
-                  />
-                );
-              })}
-            </div>
-          </Card>
+        {["down", "left", "right", "up"].map((direction) => (
+          <CharacterCard direction={direction} />
         ))}
       </div>
     </Card>
