457b09a3969adfff468e1306522d1e76e965992e
ephemeralRhythm <alijradi2003@gmail.com>
2025-06-21 11:34:18 +0300

    [server] added reset character controller

--- packages/server/src/routes/user.route.ts
+++ packages/server/src/routes/user.route.ts
@@ -1,9 +1,10 @@
 import express from "express"
-import { updateMe } from "../controllers/user.controller" 
+import { resetCharacter, updateMe } from "../controllers/user.controller" 
 
 const userRouter = express.Router()
 
 // userRouter.post("/gear", editUserGear)
 userRouter.post("/me", updateMe)
+userRouter.delete("/me", resetCharacter)
 
 export default userRouter
