f8c85f67ddf63ed86103b85ed87f00e69b3b7233
alijradii <alijradi2003@gmail.com>
2025-06-27 09:39:46 +0300

    [server] added mob goap agent

---
+++ packages/server/src/game/goap/agents/mobGaopAgent.ts
@@ -0,0 +1,42 @@
+import { GoapAgent } from "../core/goapAgent";
+import { AttackAction } from "../actions/attackAction";
+import { FollowEntityAction } from "../actions/followEntityAction";
+import { EnemyProximitySensor } from "../sensors/enemyProximitySensor";
+import { Goal } from "../core/goal";
+import { Action } from "../core/action";
+import { Entity } from "../../entities/entity";
+
+export class MobGoapAgent extends GoapAgent {
+  constructor(entity: Entity) {
+    super(entity);
+    this.sensors.push(new EnemyProximitySensor(600, 200));
+  }
+
+  override updateGoals() {
+    this.goals = [];
+
+    const enemyId = this.worldState["enemy_id"];
+    this.goals.push(new Goal("idle", 1, { state: "idle" }, this.entity));
+
+    if (enemyId === undefined) return;
+
+    this.goals.push(
+      new Goal(`attack_${enemyId}`, 5, { [`attack_${enemyId}`]: true }, this.entity)
+    );
+  }
+
+  override updateActions() {
+    const entities = this.entity.world.getAllEntities();
+    this.actions = [];
+    this.actions.push(new Action("idle", 1, {}, { state: "idle" }));
+
+    const enemyId = this.worldState["enemy_id"];
+    if (!enemyId) return;
+
+    const target = entities.find((a) => a.id === enemyId);
+    if (target) {
+      this.actions.push(new FollowEntityAction(this.entity, target, 4));
+      this.actions.push(new AttackAction(this.entity, target, this.entity.autoAttack));
+    }
+  }
+}
