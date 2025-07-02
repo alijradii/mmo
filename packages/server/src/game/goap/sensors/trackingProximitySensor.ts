cc22ba347761336dbeac01c1354cfc5325b8cd5a
alijradii <alijradi2003@gmail.com>
2025-07-02 12:18:16 +0300

    [server] added tracking logic to tracking proximity sensor

--- packages/server/src/game/goap/sensors/trackingProximitySensor.ts
+++ packages/server/src/game/goap/sensors/trackingProximitySensor.ts
@@ -1,9 +1,7 @@
 import type { Sensor } from "./sensor";
 import type { Entity } from "../../entities/entity";
 import type { WorldState } from "../core/worldState";
-import {
-  getEuclideanDistance,
-} from "../../../utils/math/helpers";
+import { getEuclideanDistance } from "../../../utils/math/helpers";
 
 export class TrackingProximitySensor implements Sensor {
   private readonly detectionRange: number;
@@ -15,54 +13,37 @@ export class TrackingProximitySensor implements Sensor {
   }
 
   update(worldState: WorldState, self: Entity, others: Entity[]): void {
-    const allies = others
-      .filter(
-        (e) =>
-          e.id !== self.id &&
-          e.party === self.party &&
-          getEuclideanDistance({ ax: self.x, ay: self.y, bx: e.x, by: e.y }) <=
-            this.detectionRange
-      )
-      .sort(
-        (a, b) => a.HP / (a.finalStats.HP || 1) - b.HP / (b.finalStats.HP || 1)
-      );
+    if (!worldState["is_tracking"]) {
+      return;
+    }
 
-    const lowestHpAlly = allies[0];
+    const entities = others.filter((e) => e.id !== self.id);
+    const trackedId = worldState["tracked_id"];
 
-    if (lowestHpAlly) {
-      const dx = lowestHpAlly.x - self.x;
-      const dy = lowestHpAlly.y - self.y;
-      const dist = Math.sqrt(dx * dx + dy * dy);
-      const hpPercent =
-        (100 * lowestHpAlly.HP) / (lowestHpAlly.finalStats.HP || 1);
+    if (!trackedId) return;
 
-      worldState["ally_detected"] = true;
-      worldState["ally_id"] = lowestHpAlly.id;
-      worldState[`distance_${lowestHpAlly.id}`] = dist;
-      worldState["ally_hp_percent"] = hpPercent;
+    const trackedEntity = entities.find((e) => e.id === trackedId);
 
-      if (dist <= 12) {
-        worldState[`within_bounds_${lowestHpAlly.id}`] = true;
-        worldState[`within_range_${lowestHpAlly.id}`] = true;
-        worldState[`within_sight_${lowestHpAlly.id}`] = true;
-      } else if (dist <= this.impactRange) {
-        worldState[`within_bounds_${lowestHpAlly.id}`] = false;
-        worldState[`within_range_${lowestHpAlly.id}`] = true;
-        worldState[`within_sight_${lowestHpAlly.id}`] = true;
-      } else if( dist <= 500){
-        worldState[`within_bounds_${lowestHpAlly.id}`] = false;
-        worldState[`within_range_${lowestHpAlly.id}`] = false;
-        worldState[`within_sight_${lowestHpAlly.id}`] = true;
-      }
-      else {
-        worldState[`within_bounds_${lowestHpAlly.id}`] = false;
-        worldState[`within_range_${lowestHpAlly.id}`] = false;
-        worldState[`within_sight_${lowestHpAlly.id}`] = false;
-      }
-    } else {
-      worldState["ally_detected"] = false;
-      delete worldState["ally_id"];
-      delete worldState["ally_hp_percent"];
+    if (!trackedEntity) {
+        worldState["is_tracking"] = false;
+        delete worldState["tracked_id"];
+        return;
+    }
+
+    const dx = trackedEntity.x - self.x;
+    const dy = trackedEntity.y - self.y;
+
+    const dist = Math.sqrt(dx * dx + dy * dy);
+
+    if (dist <= 12) {
+      worldState[`within_bounds_${trackedEntity.id}`] = true;
+      worldState[`within_range_${trackedEntity.id}`] = true;
+    } else if (dist <= this.impactRange) {
+      worldState[`within_bounds_${trackedEntity.id}`] = false;
+      worldState[`within_range_${trackedEntity.id}`] = true;
+    } else if (dist <= this.detectionRange) {
+      worldState[`within_bounds_${trackedEntity.id}`] = false;
+      worldState[`within_range_${trackedEntity.id}`] = false;
     }
   }
 
