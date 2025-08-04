fe43bf28c0e391785bab2a8018d2cb3dcdd2d5ae
alijradii <alijradi2003@gmail.com>
2025-08-04 21:29:49 +0300

    [server] added entity field to game item to fix colyseus warning

--- packages/server/src/game/core/gameItem.ts
+++ packages/server/src/game/core/gameItem.ts
@@ -1,7 +1,9 @@
 import { dataStore } from "../../data/dataStore";
 import { InventoryItem } from "../items/inventoryItem";
 import { GameObject } from "./gameObject";
+import {entity} from "@colyseus/schema"
 
+@entity
 export class GameItem extends GameObject {
     item: InventoryItem;
 
