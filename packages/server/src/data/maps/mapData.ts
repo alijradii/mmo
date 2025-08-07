ce10b873fd24d604eea63c4e6a97a3db0fd509a0
alijradii <alijradi2003@gmail.com>
2025-08-07 15:07:44 +0300

    [server] added spawn regions for dungeon map

--- packages/server/src/data/maps/mapData.ts
+++ packages/server/src/data/maps/mapData.ts
@@ -48,11 +48,108 @@ export const MAPS_DATA: Record<string, MapData> = {
         x2: 1444,
         y2: 2822,
         entities: [
+          { mob: "SkeletonWarrior", count: 4 },
+          { mob: "Bat", count: 5 },
+        ],
+      },
+      {
+        name: "left entrace isle",
+        x1: 870,
+        y1: 2423,
+        x2: 1350,
+        y2: 2511,
+        entities: [
+          { mob: "SkeletonArcher", count: 3 },
           { mob: "SkeletonWarrior", count: 2 },
-          { mob: "Bat", count: 3 },
+        ],
+      },
+      {
+        name: "left entrace room cell 1",
+        x1: 1136,
+        y1: 2246,
+        x2: 1227,
+        y2: 2317,
+        entities: [{ mob: "SkeletonArcher", count: 1 }],
+      },
+      {
+        name: "left entrace room cell 2",
+        x1: 907,
+        y1: 2256,
+        x2: 976,
+        y2: 2331,
+        entities: [{ mob: "SkeletonArcher", count: 1 }],
+      },
+      {
+        name: "left entrace room",
+        x1: 537,
+        y1: 2368,
+        x2: 795,
+        y2: 2504,
+        entities: [
+          { mob: "SkeletonArcher", count: 2 },
+          { mob: "SkeletonWarrior", count: 2 },
+          { mob: "SkeletonAssassin", count: 2 },
+        ],
+      },
+      {
+        name: "right entrance room isle",
+        x1: 1642,
+        y1: 2269,
+        x2: 1972,
+        y2: 2330,
+        entities: [{ mob: "bat", count: 8 }],
+      },
+      {
+        name: "right entrance room",
+        x1: 2037,
+        y1: 2230,
+        x2: 2304,
+        y2: 2363,
+        entities: [
+          { mob: "SkeletonBerserker", count: 3 },
+          { mob: "SkeletonAssassin", count: 1 },
+        ],
+      },
+      {
+        name: "path fork",
+        x1: 1338,
+        y1: 1983,
+        x2: 1500,
+        y2: 2031,
+        entities: [
+          {
+            mob: "SkeletonBerserker",
+            count: 1,
+          },
+          {
+            mob: "SkeletonArcher",
+            count: 2,
+          }
+        ],
+      },
+      {
+        name: "upper left isle",
+        x1: 588,
+        y1: 1400,
+        x2: 1270,
+        y2: 1485,
+        entities: [
+          {
+            mob: "SkeletonBerserker",
+            count: 2,
+          },
+          {
+            mob: "SkeletonAssassin",
+            count: 2,
+          },
+          {
+            mob: "SkeletonArcher",
+            count: 2,
+          }
         ],
       },
     ],
+
     spawnPoint: { x: 1350, y: 3200 },
   },
 };
